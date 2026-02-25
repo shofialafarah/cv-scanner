92a81d9a4aab8f8a3a375915bd7b4bfc6211ecf9
shofialafarah <shofia.lafarah74@gmail.com>
2026-02-25 08:53:32 +0700

    Install laravel breeze untuk sistem autentikasi

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CandidateController;
 use App\Http\Controllers\JobPostController;
+use App\Http\Controllers\CandidateController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/candidates', [CandidateController::class, 'index'])->name('candidates.index');
-Route::get('/candidates/create', [CandidateController::class, 'create'])->name('candidates.create');
-Route::post('/candidates', [CandidateController::class, 'store'])->name('candidates.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/jobs', [JobPostController::class, 'index'])->name('jobs.index');
-Route::get('/jobs', [JobPostController::class, 'show'])->name('jobs.show');
-Route::get('/jobs/create', [JobPostController::class, 'create'])->name('jobs.create');
-Route::post('/jobs', [JobPostController::class, 'store'])->name('jobs.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// --- AREA HR ---
+// Hanya User yang sudah Login DAN punya role 'hr' yang bisa akses
+Route::middleware(['auth', 'role:hr'])->group(function () {
+    Route::resource('jobs', JobPostController::class);
+});
+
+// --- AREA KANDIDAT ---
+// User biasa yang sudah login bisa apply
+Route::middleware(['auth'])->group(function () {
+    Route::get('/apply/{job_post_id}', [CandidateController::class, 'create'])->name('candidates.create');
+    Route::post('/apply', [CandidateController::class, 'store'])->name('candidates.store');
+});
+require __DIR__.'/auth.php';
